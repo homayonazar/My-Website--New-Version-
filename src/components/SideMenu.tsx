2eb39936f1a327c0d9994210b4ba7bcd98e8075f
homayonazar <amirrezaabdollahi01@gmail.com>
2025-09-28 21:08:53 +0300

    working in side menu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,7 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
 
 function SideMenu() {
   return (
-    <div>SideMenu</div>
+    <div>
+      <div className="fixMenu fixed w-[270px] h-full flex flex-col items-center bg-[var(--bgside)]">
+        <h1 className="mt-15 text-2xl">HOMAYONAZAR</h1>
+        <br /><br />
+        <ul className="space-y-4 text-lg">
+          <li className="flex items-center gap-2">
+            <FontAwesomeIcon icon={faHouse} /> Home
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import SideMenu from "../components/SideMenu";
 
-function Home() {
+export default function Home() {
+
+    const [darkMode, setDarkMode] = useState(() => {
+        const saved = localStorage.getItem("theme");
+        return saved ? saved === "dark" : false;
+    });
+
+    useEffect(() => {
+        const root = document.documentElement;
+        if (darkMode) {
+            root.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            root.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+        }
+    }, [darkMode]);
+
+
+
+
+
   return (
-    <div>Home</div>
+    <div>
+      <SideMenu />
+
+      <div className="ml-[270px] p-4 ">
+        <h1>Test for home page</h1>
+        <p>under main text sample</p>
+      </div>
+    </div>
   )
 }
-
-export default Home
